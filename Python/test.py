41a3a0609d6463d9579a7457d2758b435f59b893
ShebinPaul <48907562+ShebinPaul@users.noreply.github.com>
2022-08-16 18:30:09 +0530

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -1,12 +1,12 @@
 import mysql.connector
 import os
-os.system("mysql -h ivncdwdevdb1.cprqglsudqxv.us-east-1.rds.amazonaws.com -u ivncdwadm -p ivncdwadmin&2022")
+mydb = os.system("mysql -h ivncdwdevdb1.cprqglsudqxv.us-east-1.rds.amazonaws.com -u ivncdwadm -p ivncdwadmin&2022")
 # mydb=mysql.connector.connect(host="ivncdwdevdb1.cprqglsudqxv.us-east-1.rds.amazonaws.com",user="ivncdwadm",passwd="ivncdwadmin&2022")
-# mycursor=mydb.cursor() #cursor object provides connection between sql database and sql queries
-# if(mydb):
-#     print("connection successful")
-# else:
-#     print("connection unsuccessful")
-# mycursor.execute("show databases")
-# for i in mycursor:
-#     print(i)
+mycursor=mydb.cursor() #cursor object provides connection between sql database and sql queries
+if(mydb):
+    print("connection successful")
+else:
+    print("connection unsuccessful")
+mycursor.execute("show databases")
+for i in mycursor:
+    print(i)
