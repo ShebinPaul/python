c53cf90390824ebd4e1c95820c484c1d99333ec9
ShebinPaul <48907562+ShebinPaul@users.noreply.github.com>
2022-08-16 17:00:12 +0530

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb=mysql.connector.connect(host="*",user="ivnctestadmin",passwd="testadmin!202022")
+mydb=mysql.connector.connect(host="ivnc-test-rds.cprqglsudqxv.us-east-1.rds.amazonaws.com",user="ivnctestadmin",passwd="testadmin!202022")
 mycursor=mydb.cursor() #cursor object provides connection between sql database and sql queries
 if(mydb):
     print("connection successful")
