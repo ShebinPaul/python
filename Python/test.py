36bdae08c313cae4ed1db66f1019239f59885b6a
ShebinPaul <48907562+ShebinPaul@users.noreply.github.com>
2022-08-17 10:45:20 +0530

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -6,12 +6,12 @@ import boto3
 import os
 import pandas
  
-ENDPOINT="ivncdwdevdb1.cprqglsudqxv.us-east-1.rds.amazonaws.com"
+ENDPOINT="ivnc-test-rds.cprqglsudqxv.us-east-1.rds.amazonaws.com"
 PORT="3306"
-USER="ivncdwccfdev1"
+USER="ivnctestadmin"
 REGION="us-east-1"
-DBNAME="ccf_edp_audit_information"
-PWORD="Developer@123dev"
+#DBNAME="ccf_edp_audit_information"
+PWORD="testadmin!202022"
 os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
  
 # Get credentials from .aws/credentials
@@ -22,8 +22,8 @@ client = boto3.client('rds')
 token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)
  
 try:
-    # conn =  mysql.connector.connect(host=ENDPOINT, user=USER, passwd=token, port=PORT, database=DBNAME, ssl_ca='SSLCERTIFICATE')
-    conn =  mysql.connector.connect(host=ENDPOINT, user=USER, passwd=PWORD, port=PORT, database=DBNAME)
+    # conn =  mysql.connector.connect(host=ENDPOINT, user=USER, passwd=token, port=PORT, database=DBNAME, ssl_ca='SSLCERTIFICATE')#database=DBNAME
+    conn =  mysql.connector.connect(host=ENDPOINT, user=USER, passwd=PWORD, port=PORT)
     print('Connected to RDS DB')
     qrDF = pandas.read_sql('select * from ctl_dataset_master limit 2', conn)
     # cur = conn.cursor()
