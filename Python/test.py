dc8ca7aa91aaf19651abeb5f026d9de2f13104c0
ShebinPaul <48907562+ShebinPaul@users.noreply.github.com>
2022-08-17 09:52:33 +0530

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -1,12 +1,34 @@
+# RDS Connection
+# --------------
 import mysql.connector
+import sys
+import boto3
 import os
-mydb = os.system("mysql -h ivncdwdevdb1.cprqglsudqxv.us-east-1.rds.amazonaws.com -u ivncdwadm -p ivncdwadmin&2022")
-# mydb=mysql.connector.connect(host="ivncdwdevdb1.cprqglsudqxv.us-east-1.rds.amazonaws.com",user="ivncdwadm",passwd="ivncdwadmin&2022")
-mycursor=mydb.cursor() #cursor object provides connection between sql database and sql queries
-if(mydb):
-    print("connection successful")
-else:
-    print("connection unsuccessful")
-mycursor.execute("show databases")
-for i in mycursor:
-    print(i)
+import pandas
+ 
+ENDPOINT="ivncdwdevdb1.cprqglsudqxv.us-east-1.rds.amazonaws.com"
+PORT="3306"
+USER="ivncdwccfdev1"
+REGION="us-east-1"
+DBNAME="ccf_edp_audit_information"
+PWORD="Developer@123dev"
+os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
+ 
+# Get credentials from .aws/credentials
+session = boto3.Session(profile_name='default')
+client = session.client('rds')
+ 
+token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)
+ 
+try:
+    # conn =  mysql.connector.connect(host=ENDPOINT, user=USER, passwd=token, port=PORT, database=DBNAME, ssl_ca='SSLCERTIFICATE')
+    conn =  mysql.connector.connect(host=ENDPOINT, user=USER, passwd=PWORD, port=PORT, database=DBNAME)
+    print('Connected to RDS DB')
+    qrDF = pandas.read_sql('select * from ctl_dataset_master limit 2', conn)
+    # cur = conn.cursor()
+    # qr = conn.execute('select * from ctl_dataset_master limit 2')
+    # qrDF = pandas.DataFrame(qr.fetchall())
+    # qrDF.columns = qr.keys()
+    print(qrDF)
+except Exception as e:
+    print("Database connection failed due to {}".format(e))
